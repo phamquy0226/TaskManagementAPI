57deebbfd0cb06ae6e505d0bd27f645544448258
Phạm Đình Quý <148611855+phamquy0226@users.noreply.github.com>
2025-07-23 09:13:23 +0700

    ss

--- QuanLyCongViecAPI/Services/DepartmentService.cs
+++ QuanLyCongViecAPI/Services/DepartmentService.cs
@@ -29,7 +29,7 @@ namespace QuanLyCongViecAPI.Services
             }
             else
             {
-                return new ResponseModel { Success = false, Message = result.Item2, ErrorCode = -1001 };
+                return new ResponseModel { Success = false, Message = result.Item2, ErrorCode = -1101 };
             }
         }
 
